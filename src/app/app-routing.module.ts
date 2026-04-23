88d0556a8cd7db4cb5c14a29be84d2294245f417
Mohamed Aziz Ben Said <bensaidmohamedaziz1@gmail.com>
2026-04-23 21:44:09 +0200

    feat: implement responsive navigation bar and footer in app layout

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListComponent } from './etudiants/list/list.component';
+import { FormComponent } from './etudiants/form/form.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'etudiants', pathMatch: 'full' },
+  { path: 'etudiants', component: ListComponent, data: { title: 'Liste des etudiants' } },
+  { path: 'etudiants/add', component: FormComponent, data: { title: 'Ajouter un etudiant' } },
+  { path: 'etudiants/edit/:id', component: FormComponent, data: { title: 'Modifier etudiant' } },
+  // Catch-all route
+  { path: '**', redirectTo: 'etudiants' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
